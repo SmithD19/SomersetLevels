91f37d707864c538bbf7fe5d37cee010ea65325f
Daniel Smith <44264537+SmithD19@users.noreply.github.com>
2020-07-20 11:00:59 +0100

    Updated CoverWEightings.R
    Now averages cover weights at the site level rather than plot/site. This prevents ellenberg values of 0 being present when there is no cover ata particular plot because this doesnt represent the actual variables

--- Scripts/CoverWeightings.R
+++ Scripts/CoverWeightings.R
@@ -1,6 +1,6 @@
 ## ---------------------------
 ##
-## Script name: 
+## Script name:
 ##
 ## Purpose of script:
 ##
@@ -13,7 +13,7 @@
 ## ---------------------------
 ##
 ## Notes:
-##   
+##
 ##
 ## ---------------------------
 
@@ -24,13 +24,13 @@ library(tidyverse)
 ## ---------------------------
 
 # Fertility(lowest value = lowest fertility)
-# pH(lowest value = lowest pH) 
-# Wetness(lowest value = driest conditions) 
-# Light(lowest scores = most shade tolerant) 
+# pH(lowest value = lowest pH)
+# Wetness(lowest value = driest conditions)
+# Light(lowest scores = most shade tolerant)
 
-# E = Ellenberg score for each species 
-# c = cover value for each species 
-# n = number of species having Ellenbergscores in the quadrat 
+# E = Ellenberg score for each species
+# c = cover value for each species
+# n = number of species having Ellenbergscores in the quadrat
 # Cover-weighted score = Sum (E. c)/ Sum (c)
 
 # coverweighted <- function(E, C) {
@@ -48,7 +48,7 @@ vegdata <-
 cweight <- vegdata %>%
   # Data into longer format
   pivot_longer(
-    cols = c(-Plot_ID,-Year,-Season),
+    cols = c(-Plot_ID, -Year, -Season),
     names_to = "VariableName",
     values_to = "Cover"
   ) %>%
@@ -59,14 +59,34 @@ cweight <- vegdata %>%
   # Split into cover variables
   mutate(VarType = if_else(str_detect(VariableName, "cover"), "cover", "height")) %>%
   # Only take values with Cover values over 0 for performance
-  filter(VarType == "cover") %>% 
+  filter(VarType == "cover") %>%
   # Categorise variables
   mutate(Position = case_when(
-  str_detect(VariableName, "Proximal") ~ "Proximal",
-  str_detect(VariableName, "Central") ~ "Central",
-  str_detect(VariableName, "Distal") ~ "Distal")) %>% 
-  # Grouping
-  group_by(Plot_ID, Year, Season) %>% 
+    str_detect(VariableName, "Proximal") ~ "Proximal",
+    str_detect(VariableName, "Central") ~ "Central",
+    str_detect(VariableName, "Distal") ~ "Distal"
+  )) %>%
+  
+  # EDITS here ---------
+
+# We have to move from plot_Id up to a site level average for ellenberg values
+# this prevents the ellenberg values becoming 0 because there is no cover at a particular plot
+mutate(Plot_ID = as.character(Plot_ID)) %>%
+  # Seperation into the site grouping
+  separate(
+    col = Plot_ID,
+    into = c("Site", "Plot"),
+    sep = "0",
+    remove = F
+  ) %>%
+  
+  # End EDITS ---------
+
+# Grouping
+
+group_by(Site, Year, Season) %>%
+  
+  
   # Calculate the cover weighted values
   summarise(
     CW_Light = round(sum(light * Cover, na.rm = T) / sum(Cover, na.rm = T)),
@@ -76,22 +96,4 @@ cweight <- vegdata %>%
     CW_Salt = round(sum(salt * Cover, na.rm = T) / sum(Cover, na.rm = T))
   )
 
-cweight %>% write_csv("Data/CoverWeightedEllenbergs.csv")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+cweight %>% write_csv("Data/CoverWeightedEllenbergs2.csv")
