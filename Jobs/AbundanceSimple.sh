7b8a6308572218ff3804f7f5a1d4ee06ba74a66f
Dan <dansmi@ceh.ac.uk>
2020-11-02 14:22:39 +0000

    New covariate selection

---
+++ Jobs/AbundanceSimple.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --output=R-%x.%j.out
+#SBATCH --error=R-%x.%j.err
+
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=4  
+#SBATCH --threads-per-core=2
+
+# time limit and memory allocation 
+#SBATCH --time=3-00:00:00 
+#SBATCH --mem=1G 
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=d.smith34178@gmail.com
+
+# Execute
+Rscript Scripts/Abundancesimple.R
